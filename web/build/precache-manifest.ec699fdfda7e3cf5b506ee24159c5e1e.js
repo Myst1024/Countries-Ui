5d24d7a89ea352a10e5e86ee982d14ea701b97e0
Gunnar Carlson <gunnar.carlson@gmail.com>
2019-06-23 21:27:23 -0500

    Committing build folder for easier deployment

---
+++ web/build/precache-manifest.ec699fdfda7e3cf5b506ee24159c5e1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4cf7a85f3aa6f692d4de71d1773820",
+    "url": "/web/build/index.html"
+  },
+  {
+    "revision": "9c58f94a0460a1d76daa",
+    "url": "/web/build/static/css/main.afe3baa7.chunk.css"
+  },
+  {
+    "revision": "f2f77a4556d28c6b5ab2",
+    "url": "/web/build/static/js/2.fb82c2ed.chunk.js"
+  },
+  {
+    "revision": "9c58f94a0460a1d76daa",
+    "url": "/web/build/static/js/main.90da1785.chunk.js"
+  },
+  {
+    "revision": "741c64616e1a76802fcd",
+    "url": "/web/build/static/js/runtime~main.a288cbb5.js"
+  }
+]);
